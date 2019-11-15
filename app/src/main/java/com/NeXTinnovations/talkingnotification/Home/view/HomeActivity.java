7688d185d51e0e9922292691a1853a0fe1000151
@pgaurav72 <gaurav.pandit@binarynumbers.io>
2019-11-15 22:38:53 +0530

    Unversioned Files Added

---
+++ app/src/main/java/com/NeXTinnovations/talkingnotification/Home/view/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.NeXTinnovations.talkingnotification.Home.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.NeXTinnovations.talkingnotification.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
